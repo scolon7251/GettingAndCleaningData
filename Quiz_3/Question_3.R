5bef3649a9cddc438c74ac44c2b34209eff9907c
Sam Colon <scolon725@gmail.com>
2016-01-10 18:34:15 -0500

    Update Question_3.R

--- Quiz_3/Question_3.R
+++ Quiz_3/Question_3.R
@@ -1,4 +1,21 @@
-##Question 3, GDP Calculation
+##Question 3
+
+#############################################################################################
+# Load the Gross Domestic Product data for the 190 ranked countries in this data set:     
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+# 
+# Load the educational data from this data set:   
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv 
+#
+# Match the data based on the country shortcode. 
+# How many of the IDs match? 
+# Sort the data frame in descending order by GDP rank. 
+# What is the 13th country in the resulting data frame? 
+#
+# Original data sources: 
+#   http://data.worldbank.org/data-catalog/GDP-ranking-table 
+#   http://data.worldbank.org/data-catalog/ed-stats
+#############################################################################################
 
 #GDP Data--download CSV and put into object gdp_data
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv" 
